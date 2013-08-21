4ab715558e9864e00883036799e58d1c422b14ed
Theodore Cleveland <tcclevela@gmail.com>
2013-08-21 10:01:33 -0400

    testing complete

--- quiz.rb
+++ quiz.rb
@@ -11,7 +11,14 @@
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
-def sleep_in?
+def sleep_in?(options={})
+	day_of_week = Date.today.cwday
+	if day_of_week == 6 || day_of_week == 7
+		return true
+	elsif options[:vacation] && options[:vacation] == true
+		return true
+	end
+	false
 end
 
 # Question 2: a function called del_del
@@ -19,6 +26,9 @@ end
 # Remove "del" from a string.
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
+def del_del(stringystring) 
+	stringystring.delete 'del'
+end
 
 # Question 3: a function called missingChar
 #############
@@ -26,6 +36,15 @@ end
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
+
+def missing_char(stringy, index)
+	if stringy.class == String
+		stringy[index] = ''
+		return stringy
+	else
+		raise 'RuntimeError'
+	end
+end
 
 # Question 4: a function called nearHundred
 #############
@@ -36,8 +55,29 @@ end
 # near_hundred(52) => false
 # near_hundred('two') => RuntimeError: Please enter a number!
 
+def near_hundred(number)
+	if number.class == Fixnum
+		diff = 100 - number
+		return diff >= 1 && diff <= 10
+	else
+		raise 'RuntimeError: Please enter a number!'
+	end
+end
+
 # Question 5: a method called back_around
 #############
 # Given a string, move the last character to the beginning.
 # "cat".back_around => "tca"
 # "hello".back_around => "ohell"
+
+def back_around(stringy)
+	if stringy.class == String
+		last_idx = stringy.length-1
+		char = stringy[last_idx]
+		stringy[last_idx] = ''
+		stringy.prepend(char)
+		return stringy
+	else
+		raise 'RuntimeError'
+	end
+end
--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -24,6 +24,7 @@ describe "question 1: a function called sleep_in?" do
     question_1 = sleep_in?(vacation: true)
     expect(question_1).to be(true)
   end
+  
 end
 
 # Write the next tests yourself!
@@ -33,6 +34,19 @@ end
   # Example Usage:
   # del_del("abdelcd") => "abcd"
   # del_del("xyz") => "xyz"
+describe "question 2: a function called del_del" do
+
+  it 'removes the sequence "del" from an input string' do 
+    question_2 = del_del("abdelcd")
+    expect(question_2).to eq('abc')
+  end
+
+  it 'does not remove anything from a string void of "del"' do 
+    question_2 = del_del('xyz')
+    expect(question_2).to eq('xyz')
+  end
+
+end 
 
 # Question 3: a function called missingChar
   # Example Usage:
@@ -40,13 +54,72 @@ end
   # missing_char(347, 1) => RuntimeError: Please enter a string!
   # https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/expect-error
 
+describe "question 3: a function called missingChar" do
+  
+  it 'should remove a letter from a string at the input index' do
+    question_3 = missing_char('kitten', 1)
+    expect(question_3).to eq('ktten')
+  end
+
+  it 'should error out if a non-string is input' do 
+    expect{missing_char(3465, 1)}.to raise_error(RuntimeError)
+  end
+
+  it 'should only cut the letter at the index, not all of the same letter' do
+    question_3 = missing_char('kickass', 5)
+    expect(question_3).to eq('kickas') 
+  end
+
+end 
+
+
 # Question 4: a function called nearHundred
   # Example Usage:
   # 93.near_hundred? => true
   # near_hundred(52) => false
   # near_hundred('two') => RuntimeError: Please enter a number!
 
+describe 'question 4: near_hundred should return true if number is > 89 and <100' do
+  
+  it 'should return true for 90->99' do 
+    result = true
+    number = 90
+    while(result && number < 100)
+      result = near_hundred(number)
+      number+=1
+    end
+    expect(result).to eq(true)
+  end
+
+  it 'should return false if number is not in domain' do
+    result = near_hundred(1000)
+    expect(result).to eq(false)
+  end
+
+  it 'should raise an error if a number is not passed' do 
+    expect{near_hundred('two')}.to raise_error('RuntimeError: Please enter a number!')
+  end
+
+end
+
 # Question 5: a method called back_around
   # Example Usage:
   # "cat".back_around => "tca"
   # "hello".back_around => "ohell"
+describe 'question 5: it should move the last letter to the start' do
+
+  it 'should move the last letter to the beginning of the word' do 
+    result = back_around('hello')
+    expect(result).to eq('ohell')
+  end
+  
+  it 'should return single characters if single characters are input' do 
+    result = back_around('h')
+    expect(result).to eq('h')
+  end
+  
+  it 'should error out if number is input' do
+    expect{back_around(123).to raise_error('RuntimeError')}
+  end
+
+end
