8979786434f3fcd57cdd53373f50df956bc22dc1
Zachariah Reitano <zreitano@gmail.com>
2013-08-21 09:59:22 -0400

    One test for each ? from 2-5

--- quiz.rb
+++ quiz.rb
@@ -11,7 +11,22 @@
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
-def sleep_in?
+options = {
+  vacation: false
+}
+def sleep_in?(options)
+  weekend = ["saturday", "sunday"]
+  weekedays = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+ if options[:vacation] == true
+    return true
+  else
+    if weekend.include?(Time.now.strftime("%A"))
+      return true
+    else
+      return false
+    end
+ end
 end
 
 # Question 2: a function called del_del
@@ -20,12 +35,24 @@ end
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
 
+phrase = "abdelcd"
+def del_del(phrase)
+  phrase.gsub("del", "")
+end
+
 # Question 3: a function called missingChar
 #############
 # Remove the character that corresponds to the index from the string.
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
+
+phrase = "hello"
+number = 2
+
+def missingChar(phrase, number)
+  phrase.sub(phrase[number], "")
+end
 
 # Question 4: a function called nearHundred
 #############
@@ -36,8 +63,32 @@ end
 # near_hundred(52) => false
 # near_hundred('two') => RuntimeError: Please enter a number!
 
+number = 99
+def nearHundred(number)
+  if number.between?(90, 110)
+    return true
+  else
+    return false
+  end
+end
+
+
 # Question 5: a method called back_around
 #############
 # Given a string, move the last character to the beginning.
 # "cat".back_around => "tca"
 # "hello".back_around => "ohell"
+
+word = "lemon"
+def back_around(word)
+  letter = word[-1]
+  letter_removed_word = word.sub(letter, "")
+  return changed_word = "#{letter}" + letter_removed_word
+end
+
+
+
+
+
+
+
